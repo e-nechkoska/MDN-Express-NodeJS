d61c301097563cc34cce99d22411219c41d9037c
e-nechkoska <e.nechkoska@gmail.com>
2020-06-04 11:48:32 +0200

    fix typo

--- Local-library/models/author.js
+++ Local-library/models/author.js
@@ -20,7 +20,7 @@ let AuthorSchema = new Schema({
 AuthorSchema.virtual("name").get(function () {
   let fullname = "";
   if (this.first_name && this.family_name) {
-    fullname = this.family + "," + this.first_name;
+    fullname = this.family_name + "," + this.first_name;
   }
   if (!this.first_name || !this.family_name) {
     fullname = "";
