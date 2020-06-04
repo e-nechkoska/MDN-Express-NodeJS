c7d672d7f742c50942070ec0491b499ecf5cbfd8
e-nechkoska <e.nechkoska@gmail.com>
2020-06-04 11:48:20 +0200

    add bookinstance_list controller

--- Local-library/controllers/bookinstanceController.js
+++ Local-library/controllers/bookinstanceController.js
@@ -1,7 +1,14 @@
 let BookInstance = require('../models/bookinstance');
 
-let bookinstance_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance list');
+let bookinstance_list = function(req, res, next) {
+  BookInstance.find()
+  .populate('book')
+  .exec(function (err, list_bookinstances) {
+    if(err) {
+      return next(err);
+    }
+    res.render('bookinstance_list', {title: 'Book Instance List', bookinstance_list: list_bookinstances});
+  });
 };
 
 let bookinstance_detail = function(req, res) {
