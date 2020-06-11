7d921a10707a8996c3abc0bc679317d4c06fee93
e-nechkoska <e.nechkoska@gmail.com>
2020-06-11 13:55:55 +0200

    use helmet and compression

--- Local-library/app.js
+++ Local-library/app.js
@@ -4,6 +4,8 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let mongoose = require('mongoose');
+let compression = require('compression');
+let helmet = require('helmet');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
@@ -29,6 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(compression()); // Compress all routes
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
